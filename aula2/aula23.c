2703e48ad39dc76a66b919c76f8df67979165ecb
marcoagpegoraro <marco.ag.pegoraro@hotmail.com>
2020-09-16 20:02:01 -0300

    first commit

---
+++ aula2/aula23.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define alturaMaxima 255
+
+typedef int CHAVE;
+
+typedef struct {
+    int peso;
+    int altura;
+} PesoAltura;
+
+int main(){
+    PesoAltura* pessoa1 = (PesoAltura*) malloc(sizeof(PesoAltura));
+    pessoa1->peso = 80;
+    pessoa1->altura = 185;
+
+    printf("Peso: %d Altura: %d\n", pessoa1->peso, pessoa1->altura);
+    
+    if(pessoa1->altura > alturaMaxima) printf("altura acima da maxima\n");
+    else printf("altura abaixo da maxima\n");
+
+
+    return 0;
+}
